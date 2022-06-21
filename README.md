7f715be888bf03f9d77f8a038b3187d5d6559f46
Oussama <oussamamater@outlook.com>
2022-06-21 15:49:38 +0100

    Updated README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Process Scheduling Solver
+> A Web/CLI tool to generate Gantt Chart and calculate TAT (turnaround time) and WAT (waiting time) based on various scheduling algorithms.
+
+![preview](web-interface/preview.png)
+
+
+## Supported algorithms
+- First Come First Serve / FCFS
+- Shortest Job First / SJF (non-preemptive)
+- Shortest Remaining Time First / SRTF (preemptive)
+- Round-Robin / RR
+- Priority (non-preemptive)
+
+## Built with
+- C Programming Language
+- Laravel
+- Livewire
--- web-interface/routes/web.php
+++ web-interface/routes/web.php
@@ -1,31 +1,26 @@
 <?php
 
-use mikehaertl\shellcommand\Command;
+use App\Models\Algo;
 use Illuminate\Support\Facades\Route;
 
-
-// El output mtei CLI nformatih kima nheb
-// El output li jey ll php ykoun 3ibara ala 3 files, w ken haka nbadil directory mtehom bsh yab9a
-// C ndhif, naaml wahda feha l order, wahda feha l finish time, wahda feha l arival, priority, burst, etc..
-// namelhom 3ibara ala helpers fil C
-
-// ken haka nkhali el main mte kol algo ala wahdo ll php, fiblast yetaada b atwel haja
-// ena maw l ing? ena li nhel l machekel? hedhi ahsen solution ;)
-
-// ! maybe naaml unit test l C w nhothom fi rapport
-// nwali ken haka nshouf kifeh nzidha fazet y selection l fichier
 Route::view('/', 'app');
 
-Route::get('/test', function () {
-    // this will be game changing
-    // $command = new Command('ls /home/oussama/Desktop/Code/scheduler/c-program/algos/config.txt');
-    // if ($command->execute()) {
-    //     echo $command->getOutput();
-    // } else {
-    //     dump(empty($command->getError()));
-    // }
-    dump(config('path.name'));
-    //config(['path.name' => 'America/Chicago']);
-    dump(config('path.name'));
-    dd('done');
+Route::get('/seed', function () {
+    Algo::create([
+        'name' => 'FIFO'
+    ]);
+
+    Algo::create([
+        'name' => 'SJF'
+    ]);
+
+    Algo::create([
+        'name' => 'Round Robin'
+    ]);
+
+    Algo::create([
+        'name' => 'Priority - NP'
+    ]);
+
+    dd('success');
 });
